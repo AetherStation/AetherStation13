4a7159df184e831809ed11f88d3eaf36fb20ee67
Nitrome <nourdinneh@hotmail.fr>
2022-06-04 03:19:17 +0200

    Removed from UI

--- tgui/packages/tgui/interfaces/ChemDispenser.js
+++ tgui/packages/tgui/interfaces/ChemDispenser.js
@@ -9,8 +9,6 @@ export const ChemDispenser = (props, context) => {
   const { act, data } = useBackend(context);
   const recording = !!data.recordingRecipe;
   const { recipeReagents = [] } = data;
-  const [hasCol, setHasCol] = useLocalState(
-    context, 'has_col', false);
   // TODO: Change how this piece of shit is built on server side
   // It has to be a list, not a fucking OBJECT!
   const recipes = Object.keys(data.recipes)
@@ -43,19 +41,6 @@ export const ChemDispenser = (props, context) => {
                   Recording
                 </Box>
               )}
-              <Button
-                icon="book"
-                disabled={!data.isBeakerLoaded}
-                content={"Reaction search"}
-                tooltip={data.isBeakerLoaded ? "Look up recipes and reagents!" : "Please insert a beaker!"}
-                tooltipPosition="bottom-start"
-                onClick={() => act('reaction_lookup')} />
-              <Button
-                icon="cog"
-                tooltip="Color code the reagents by pH"
-                tooltipPosition="bottom-start"
-                selected={hasCol}
-                onClick={() => setHasCol(!hasCol)} />
             </>
           )}>
           <LabeledList>
@@ -143,10 +128,6 @@ export const ChemDispenser = (props, context) => {
                 width="129.5px"
                 lineHeight={1.75}
                 content={chemical.title}
-                tooltip={"pH: " + chemical.pH}
-                backgroundColor={recipeReagents.includes(chemical.id)
-                  ? hasCol ? "black" : "green"
-                  : hasCol ? chemical.pHCol : "default"}
                 onClick={() => act('dispense', {
                   reagent: chemical.id,
                 })} />
@@ -205,13 +186,6 @@ export const ChemDispenser = (props, context) => {
                   units of {chemical.name}
                 </Box>
               ))}
-              {((beakerContents.length > 0 && !!data.showpH) && (
-                <Box>
-                  pH:
-                  <AnimatedNumber
-                    value={data.beakerCurrentpH} />
-                </Box>)
-              )}
             </LabeledList.Item>
           </LabeledList>
         </Section>
